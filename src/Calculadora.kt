0cdfd7bb4db360cbc862638d014611848f308432
Marllon Camelo <marllon.camelo@fnde.gov.br>
2025-10-30 08:29:48 -0300

    aprendendo comentarios

--- src/Calculadora.kt
+++ src/Calculadora.kt
@@ -1,4 +1,11 @@
+// comentarios de uma linha
 fun main(args: Array<String>) {
     println("Hello, Marllon how are you!?")
-    println("I am learning Kotlin")
+    println("I am learning Kotlin") // aprendendo comentarios
 }
+/*
+* comentarios de varias linhas
+*
+* */
+fun sum(a: Int, b: Int) = a + b
+
