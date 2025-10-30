54ab9e3e7091009729dc4dd2d1e717b026b8959f
Marllon Camelo <marllon.camelo@fnde.gov.br>
2025-10-30 08:41:56 -0300

    mais metodos da CALCULADORA

--- src/Calculadora.kt
+++ src/Calculadora.kt
@@ -9,3 +9,33 @@ fun main(args: Array<String>) {
 * */
 fun sum(a: Int, b: Int) = a + b
 
+fun subtract(a: Int, b: Int): Int {
+    return a - b
+}
+/*
+* testando multiplicação
+*
+* */
+fun multiply(a: Int, b: Int) = a * b
+/*
+* testando divisão
+* */
+fun divide(a: Int, b: Int) = a / b
+/*
+* testando média
+* */
+fun mean(a: Int, b: Int) = (a + b) / 2
+/*
+* testando quadrado
+* */
+fun square(n: Int) = n * n
+/*
+* testando cubo
+* */
+fun cube(n: Int) = n * n * n
+/*
+* testando potência
+* */
+fun power(n: Int, p: Int) = Math.pow(n.toDouble(), p.toDouble()).toInt()
+
+
